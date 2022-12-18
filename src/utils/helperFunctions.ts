715f2779e6024e79468faf965d0a8f2b20135cad
David Xia <dxia@spotify.com>
2022-12-18 17:48:01 -0500

    feat: add after-tax contribution option to frontload calculator
    * add option to set whether 401k only allows integer percentage contributions
    * update some text to be more concise

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -3,20 +3,19 @@
 import { FREQUENCIES } from "./constants";
 
 /**
- * Formats numbers to string of USD currency
- * @param num number to convert to currency
- * @returns USD currency string with ',' for every 1000s place and '.' for decimal
+ * Formats numbers to string with exactly 2 decimal places
+ * @param num number to convert
+ * @returns string with ',' for every 1000s place and '.' for decimal
  */
 export const formatCurrency = (num: number): string => {
-  let formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  });
+    let formatter = new Intl.NumberFormat('en-US', {
+      style: 'decimal',
+      // These options are needed to round to whole numbers if that's what you want.
+      minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+      maximumFractionDigits: 2, // (causes 2500.99 to be printed as $2,501)
+    });
 
-  return formatter.format(num);
+    return formatter.format(num);
 };
 
 /**
@@ -26,9 +25,9 @@ export const formatCurrency = (num: number): string => {
  * @returns num * 100 then rounded (default) then appended with '%'
  */
 export const formatPercent = (num: number, round: boolean = true): string => {
-  const percentNumber = round ? Math.round(num * 100) : num * 100;
-  return percentNumber + "%";
-};
+  const percentNumber = round ? Math.round(num * 100) : (num * 100).toFixed(2);
+  return String(percentNumber);
+}
 
 /**
  * This function converts a React state of type string or number to number then string.This helps with formatting like removing preceeding 0's in input.
